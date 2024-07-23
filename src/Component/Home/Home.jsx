8d8d7a7dfe5d244b36dd80b6d8d0c5426172599e
NjatoRamah <oramahery@gmail.com>
2024-07-23 09:35:00 +0200

    swiper dashboard

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import SideBar from '../SideBar/SideBar';
 import Header from '../Header/Header';
-import Router from '../Rout/Router';
+
 import '../Page/Dashboard/Dashboard.scss'; // Importation du style pour Dashboard si nécessaire
 import '../Profil/Profil.scss'; // Importation du style pour Profil si nécessaire
+import {Routes, Route} from 'react-router-dom';
+import Dashboard from '../Page/Dashboard/Dashboard';
+import Profil from '../Profil/Profil';
 
 const Home = () => {
     const [activePage, setActivePage] = useState('Dashboard');
@@ -16,7 +19,10 @@ const Home = () => {
                     <Header activepage={activePage} />
                 </div>
                 <div className="content">
-                    <Router />
+                    <Routes>
+                        <Route path='/dashboard' element={<Dashboard/>}/>
+                        <Route path='/addCar' element={<Profil/>}/>
+                    </Routes>
                 </div>
             </div>
         </div>
