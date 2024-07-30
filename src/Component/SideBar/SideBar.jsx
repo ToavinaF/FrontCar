dbc94811d8cdfc5f3d3c74496ac89260c47bace2
Aldoleon22 <leonaldo2608@gmail.com>
2024-07-30 13:50:04 +0300

    push fotsiny

--- src/Component/SideBar/SideBar.jsx
+++ src/Component/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SideBar.scss';
 import { IoCarSport } from 'react-icons/io5';
 import { MdDashboard } from 'react-icons/md';
@@ -15,31 +15,39 @@ const languages = [
 
 const SideBar = ({ setActivePage }) => {
     const [activeDropdown, setActiveDropdown] = useState(null);
+    const [role, setRole] = useState('');
     const { t } = useTranslation();
     const currentLanguageCode = Cookies.get('i18next') || 'en';
+
+    useEffect(() => {
+        // Récupérer le rôle depuis localStorage et mettre à jour l'état
+        const storedRole = localStorage.getItem('role');
+        if (storedRole) {
+            setRole(storedRole);
+        }
+    }, []); // Le tableau vide [] signifie que ce useEffect s'exécute une seule fois après le premier rendu
 
     const handleDropdownClick = (index) => {
         setActiveDropdown(activeDropdown === index ? null : index);
     };
+
     const handleLanguageChange = (code) => {
         i18next.changeLanguage(code);
         Cookies.set('i18next', code);
         console.log('Language changed to:', code);
     };
+
     return (
         <div className='SideBar'>
             <div className="logo">
                 <IoCarSport className='icon_logo' />
-                
             </div>
 
             <div className="navlink">
                 <ul>
-
                     <NavLink to='/Home' className='li-dash' onClick={() => setActivePage('Dashboard')}>
                         <MdDashboard className='icon-dash' />
                         <p>{t('dashboard')}</p>
-                        
                     </NavLink>
 
                     <div className='li-dash'>
@@ -50,10 +58,21 @@ const SideBar = ({ setActivePage }) => {
                             </p>
                         </div>
                         <div className={`dropDown ${activeDropdown === 0 ? 'active' : ''}`}>
-                            <li className='nav_link'><NavLink className='nav_item' to={'/Home/listcar'} onClick={() => setActivePage('All Car')}>{t('all_car')}</NavLink></li>
-                            <li className='nav_link'><NavLink to={'/Home/addcar'} className='nav_item' onClick={() => setActivePage('Add Car')}>{t('add_car')}</NavLink></li>
-                            <li className='nav_link'><NavLink to={'/Home/Historique'} className='nav_item' onClick={() => setActivePage('Historiques')}>{t('Historique')}</NavLink></li>
-
+                            <li className='nav_link'>
+                                <NavLink className='nav_item' to='/Home/listcar' onClick={() => setActivePage('All Car')}>
+                                    {t('all_car')}
+                                </NavLink>
+                            </li>
+                            <li className='nav_link'>
+                                <NavLink to='/Home/addcar' className='nav_item' onClick={() => setActivePage('Add Car')}>
+                                    {t('add_car')}
+                                </NavLink>
+                            </li>
+                            <li className='nav_link'>
+                                <NavLink to='/Home/Historique' className='nav_item' onClick={() => setActivePage('Historiques')}>
+                                    {t('Historique')}
+                                </NavLink>
+                            </li>
                         </div>
                     </div>
 
@@ -65,10 +84,21 @@ const SideBar = ({ setActivePage }) => {
                             </p>
                         </div>
                         <div className={`dropDown ${activeDropdown === 1 ? 'active' : ''}`}>
-                            <li className='nav_link'><NavLink to={'/Home/listUser'} className='nav_item' onClick={() => setActivePage('All Users')}>{t('all_users')}</NavLink></li>
-                            <li className='nav_link'><NavLink to={'/Home/AjoutUser'} href="#" className='nav_item' onClick={() => setActivePage('Add User')}>{t('add_user')}</NavLink></li>
+                            <li className='nav_link'>
+                                <NavLink to='/Home/listUser' className='nav_item' onClick={() => setActivePage('All Users')}>
+                                    {t('all_users')}
+                                </NavLink>
+                            </li>
+                            {role === 'superAdmin' && ( // Correction ici
+                                <li className='nav_link'>
+                                    <NavLink to='/Home/AjoutUser' className='nav_item' onClick={() => setActivePage('Add User')}>
+                                        {t('add_user')}
+                                    </NavLink>
+                                </li>
+                            )}
                         </div>
                     </div>
+
                     <div className='li-dash'>
                         <div className='btn-lang'>
                             {languages.map(({ code, name, country_code }) => (
@@ -86,11 +116,10 @@ const SideBar = ({ setActivePage }) => {
                             ))}
                         </div>
                     </div>
-
                 </ul>
             </div>
         </div>
     );
-}
+};
 
 export default SideBar;
