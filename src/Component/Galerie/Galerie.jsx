ba59b2d9e371e74bdf85505c394d1e9f52b1b0d2
Toavina0102 <tsilatoavin@gmail.com>
2024-08-01 14:23:35 +0300

    maj aldo

--- src/Component/Galerie/Galerie.jsx
+++ src/Component/Galerie/Galerie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './Galerie.scss';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
@@ -7,84 +7,75 @@ import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
 
 const Galerie = () => {
     const { id } = useParams();
-    const [Galerie, setGalerie] = useState([]);
-    const [Open, setOpen] = useState(false);
-    const [ImageIndex, setImageIndex] = useState(0);
+    const [galerie, setGalerie] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [imageIndex, setImageIndex] = useState(0);
 
     useEffect(() => {
         fetchImg();
-    }, [id])
+    }, [id]);
 
-    //affichage boucle imgage
+    // Affichage boucle image
     const fetchImg = async () => {
         try {
-            const ViewImg = await axios.get("http://127.0.0.1:8000/api/viewGalerie/" + id);
-            setGalerie(ViewImg.data.galerie);
-            console.log(ViewImg.data.galerie);
+            const viewImg = await axios.get(`http://127.0.0.1:8000/api/viewGalerie/${id}`);
+            setGalerie(viewImg.data.galerie);
+            console.log(viewImg.data.galerie);
         } catch (error) {
-
+            console.error(error);
         }
     };
 
-    //affichage image par id clicke
-    //ouverture du fenetre
-    const OpenModal = (index) => {
+    // Ouverture du modal à l'image cliquée
+    const openModal = (index) => {
         setImageIndex(index);
         setOpen(true);
     };
-    //Fermeture du fenetre
-    const CloseModal = () => {
+
+    // Fermeture du modal
+    const closeModal = () => {
         setOpen(false);
-    }
-    //deffilement des image
+    };
+
+    // Défilement des images
     const nextImage = () => {
-        setImageIndex((ImageIndex + 1) % Galerie.length);
+        setImageIndex((imageIndex + 1) % galerie.length);
     };
 
     const prevImage = () => {
-        setImageIndex((ImageIndex + Galerie.length - 1) % Galerie.length);
+        setImageIndex((imageIndex + galerie.length - 1) % galerie.length);
     };
 
     return (
         <div className='contenaireGal'>
-            <h1>Marque du  vehicule</h1>
+            <h1>Marque du véhicule</h1>
             <h2>Description</h2>
             <div className="BlockGal">
-                {
-                    Galerie.map((image, i) => {
-                        return (
-                            <div className="BlockImg" key={i} onClick={() => OpenModal(i)}>
-                                <img src={`http://127.0.0.1:8000/storage/GalerieVehicule/${image.image}`} alt="{`Galerie image ${index}`}" />
-                            </div>
-                        )
-
-                    })
-
-                }
+                {galerie.map((image, i) => (
+                    <div className="BlockImg" key={i} onClick={() => openModal(i)}>
+                        <img src={`http://127.0.0.1:8000/storage/GalerieVehicule/${image.image}`} alt={`Galerie image ${i}`} />
+                    </div>
+                ))}
             </div>
-            {/* fenetre image */}
+
+            {/* Fenêtre image */}
             <Modal
-                isOpen={Open}
-                onRequestClose={CloseModal}
-                contentLabel="Image Modal"
-                className="image-modal"
-                overlayClassName="image-modal-overlay"
+                isOpen={open}
+                onRequestClose={closeModal}
+                className="custom-modal"
+                overlayClassName="custom-overlay"
             >
-                {/* afficge image */}
                 <div className="modal-content">
                     <div className="content-Modal">
-                        <button onClick={CloseModal} className="close-button" >X</button>
-                        {/* <button onClick={prevImage} className="nav-button prev-button">&lt;</button> */}
-                        <GrCaretPrevious className='prev-button' onClick={prevImage}/>
-                        <img src={`http://127.0.0.1:8000/storage/GalerieVehicule/${Galerie[ImageIndex]?.image}`} alt={`Galerie image ${ImageIndex}`} className="modal-image" />
-                        {/* <button onClick={nextImage} className="nav-button next-button">&gt;</button> */}
-                        <GrCaretNext className='next-button'onClick={nextImage} />
+                        <button onClick={closeModal} className="close-button">X</button>
+                        <GrCaretPrevious className='prev-button' onClick={prevImage} />
+                        <img src={`http://127.0.0.1:8000/storage/GalerieVehicule/${galerie[imageIndex]?.image}`} alt={`Galerie image ${imageIndex}`} className="modal-image" />
+                        <GrCaretNext className='next-button' onClick={nextImage} />
                     </div>
-
                 </div>
             </Modal>
         </div>
-    )
-}
+    );
+};
 
-export default Galerie
+export default Galerie;
