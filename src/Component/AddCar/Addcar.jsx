06a3334e2f29ae66cb13c77415e8d438d9fd5a07
Ralobo213 <tolotramamailalaina@gmail.com>
2024-08-07 10:20:00 +0300

    gestion erreur bolo

--- src/Component/AddCar/Addcar.jsx
+++ src/Component/AddCar/Addcar.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'; 
-import { IoCameraOutline } from "react-icons/io5"; 
-import { IoAddCircleSharp } from "react-icons/io5"; 
-import { FaXmark } from "react-icons/fa6"; 
-import { CiCircleCheck, CiTrash } from "react-icons/ci"; 
-import { useDropzone } from 'react-dropzone'; 
-import './Addcar.scss'; 
-import axios from 'axios'; 
-import { useTranslation } from 'react-i18next'; 
-import { useNavigate } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import { IoAlertCircleOutline, IoCameraOutline } from "react-icons/io5";
+import { IoAddCircleSharp } from "react-icons/io5";
+import { FaXmark } from "react-icons/fa6";
+import { CiCircleCheck, CiTrash } from "react-icons/ci";
+import { useDropzone } from 'react-dropzone';
+import './Addcar.scss';
+import axios from 'axios';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
-const Addcar = () => { 
-  const { t } = useTranslation(); 
+const Addcar = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
-  const [ShowUpload, setShowUpload] = useState(false); 
-  const [selectedImage, setSelectedImage] = useState([]); 
-  const [profile, setProfile] = useState(null); 
-  const [formData, setFormData] = useState({ 
+  const [ShowUpload, setShowUpload] = useState(false);
+  const [selectedImage, setSelectedImage] = useState([]);
+  const [profile, setProfile] = useState(null);
+  const [formData, setFormData] = useState({
     marque: '',
     matricule: '',
     description: '',
@@ -25,7 +25,7 @@ const Addcar = () => {
     place: '',
     bagage: ''
   });
-
+  const [errors, setErrors] = useState({});
   const handleImageClick = () => {
     setShowUpload(true);
   };
@@ -50,8 +50,14 @@ const Addcar = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const validationErrors = validForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+    }
     const data = new FormData();
-    data.append('marque', formData.marque); 
+    data.append('marque', formData.marque);
     data.append('matricule', formData.matricule);
     data.append('description', formData.description);
     data.append('prix', formData.prix);
@@ -66,7 +72,7 @@ const Addcar = () => {
     try {
       const response = await axios.post("http://127.0.0.1:8000/api/AddCar", data, {
         headers: {
-          'Content-Type':'multipart/form-data'
+          'Content-Type': 'multipart/form-data'
         }
       });
       localStorage.setItem('message', response.data.message);
@@ -77,11 +83,26 @@ const Addcar = () => {
       console.error(error);
     }
   };
+  //gestion erreur formulaire
+  const validForm = () => {
+    const errors = {};
+    if (!formData.marque) errors.marque = 'Ce champ est requis !';
+    if (!formData.matricule) errors.matricule = 'Ce champ est requis !';
+    if (!formData.prix) errors.prix = 'Ce champ est requis !';
+    if (!formData.bagage) errors.bagage = 'Ce champ est requis !';
+    if (!formData.place) errors.place = 'Ce champ est requis !';
+    if (!formData.porte) errors.porte = 'Ce champ est requis !';
+    if (!formData.description) errors.description = 'Ce champ est requis !';
+    if (!formData.transmission) errors.transmission = 'Ce champ est requis !';
+
+    return errors;
+  };
+  // ///////
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop: handleDrop,
-    multiple: true, 
-    accept:'image/*'
+    multiple: true,
+    accept: 'image/*'
   });
 
   return (
@@ -91,19 +112,19 @@ const Addcar = () => {
           {/* Drop zone */}
           <div className="image-upload">
             <div {...getRootProps({ className: 'dropzone' })}>
-              <input {...getInputProps()}/>
-                {selectedImage.length > 0 && (
-                  <div className="barImg">
-                    {selectedImage.map((file, index) => (
-                      <div key={index} className="partImg" onClick={(e) => e.stopPropagation()}>
-                        <div className="image-container">
-                          <img src={URL.createObjectURL(file)} alt={`Selected ${index}`} />
-                          <div className="cach"><CiTrash className='check' onClick={(e) => handleRemoveImage(index)} /></div>
-                        </div>
+              <input {...getInputProps()} />
+              {selectedImage.length > 0 && (
+                <div className="barImg">
+                  {selectedImage.map((file, index) => (
+                    <div key={index} className="partImg" onClick={(e) => e.stopPropagation()}>
+                      <div className="image-container">
+                        <img src={URL.createObjectURL(file)} alt={`Selected ${index}`} />
+                        <div className="cach"><CiTrash className='check' onClick={(e) => handleRemoveImage(index)} /></div>
                       </div>
-                    ))}
-                  </div>
-                )}
+                    </div>
+                  ))}
+                </div>
+              )}
               <p>{t('Glissez-déposez une image ici, ou cliquez pour sélectionner une image')}</p>
               <IoAddCircleSharp className='plus' />
             </div>
@@ -114,42 +135,48 @@ const Addcar = () => {
           <div className="content_form">
             <div className="parti-left">
               {/* formulaire */}
-                <div className="caracter">
-                  <label htmlFor="marque"> {t('Marque du vehicule')}</label>
-                  <input type="text" placeholder={t('Marque du vehicule')} className='input' name='marque' onChange={handleChange} />
-                </div>
-                <div className="caracter">
-                  <label htmlFor="matricule">{t('Matricule du vehicule')}</label>
-                  <input type="text" placeholder={t('Matricule du vehicule')} className='input' name='matricule' onChange={handleChange} />
-                </div>
-                <div className="caracter">
-                  <label htmlFor="transmission">{t('Type de transmission')}</label>
-                    <select name="transmission" className='input' onChange={handleChange}>
-                      <option></option>
-                      <option value="Automatique">Automatique</option>
-                      <option value="Manuelle">Manuelle</option>
-                    </select>
-                </div>
+              <div className="caracter">
+                <label htmlFor="marque"> {t('Marque du vehicule')}</label>
+                <input type="text" placeholder={t('Marque du vehicule')} className={`input ${errors.marque ? 'input-error' : ''}`} name='marque' onChange={handleChange} />
+                {/* affichage d'erreur de chaque champ */}
+                {errors.marque && <p className="error-text"><span><IoAlertCircleOutline /></span> {errors.marque}</p>}
+              </div>
+              <div className="caracter">
+                <label htmlFor="matricule">{t('Matricule du vehicule')}</label>
+                <input type="text" placeholder={t('Matricule du vehicule')} className={`input ${errors.matricule ? 'input-error' : ''}`} name='matricule' onChange={handleChange} />
+                {errors.matricule && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.matricule}</p>}
+              </div>
+              <div className="caracter">
+                <label htmlFor="transmission">{t('Type de transmission')}</label>
+                <select name="transmission" className={`input ${errors.transmission ? 'input-error' : ''}`} onChange={handleChange}>
+                  <option></option>
+                  <option value="Automatique">Automatique</option>
+                  <option value="Manuelle">Manuelle</option>
+                </select>
+                {errors.transmission && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.transmission}</p>}
+              </div>
             </div>
 
             <div className="parti-right">
               <div className="caracter">
                 <label htmlFor="prix">{t('prix du vehicule')}</label>
-                <input type="text" placeholder={t('prix du vehicule')} className='input' name='prix' onChange={handleChange} />
+                <input type="text" placeholder={t('prix du vehicule')} className={`input ${errors.prix ? 'input-error' : ''}`} name='prix' onChange={handleChange} />
+                {errors.prix && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.prix}</p>}
               </div>
               <div className="caracter">
                 <label htmlFor="bagage">{t('Nombre de bagage')}</label>
-                <select name="bagage" className='input' onChange={handleChange}>
+                <select name="bagage" className={`input ${errors.bagage ? 'input-error' : ''}`} onChange={handleChange}>
                   <option></option>
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
                 </select>
+                {errors.bagage && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.bagage}</p>}
               </div>
               <div className="caracter">
                 <label htmlFor="place">{t('Nombre de place')}</label>
-                <select name="place" className='input' placeholder='nombre de place' onChange={handleChange}>
+                <select name="place" className={`input ${errors.place ? 'input-error' : ''}`} placeholder='nombre de place' onChange={handleChange}>
                   <option></option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -157,18 +184,23 @@ const Addcar = () => {
                   <option value="5">5</option>
 
                 </select>
+                {errors.place && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.place}</p>}
+
               </div>
               <div className="caracter">
                 <label htmlFor="porte">{t('Nombre de porte')}</label>
-                <select name="porte" className='input' onChange={handleChange}>
+                <select name="porte" className={`input ${errors.porte ? 'input-error' : ''}`} onChange={handleChange}>
                   <option></option>
                   <option value="3">3</option>
                   <option value="5">5</option>
                 </select>
+                {errors.porte && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.porte}</p>}
+
               </div>
               <div className="desc_car">
                 <label htmlFor="description">{t('Apropos du vehicule')}</label>
-                <textarea id="description" className='desc' name='description' onChange={handleChange}></textarea>
+                <textarea id="description" className={`desc ${errors.description ? 'input-error' : ''}`} name='description' onChange={handleChange}></textarea>
+                {errors.description && <p className="error-text"><span><IoAlertCircleOutline /></span>{errors.description}</p>}
               </div>
               <button type='submit' className='btn'>{t('Enregistre')}</button>
             </div>
