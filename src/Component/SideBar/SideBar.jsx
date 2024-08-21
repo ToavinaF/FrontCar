582e6abc1f9de744f3429e4dc8d183af24351759
Ralobo213 <tolotramamailalaina@gmail.com>
2024-08-21 10:05:31 +0300

    modificatio pre-prod

--- src/Component/SideBar/SideBar.jsx
+++ src/Component/SideBar/SideBar.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './SideBar.scss';
 import { IoCarSport } from 'react-icons/io5';
-import { MdDashboard } from 'react-icons/md';
 import { FaChevronDown, FaCar, FaUser, FaUsers, FaCarAlt, FaArchive } from 'react-icons/fa';
 import { MdDashboard, MdDelete } from 'react-icons/md';
-import { FaChevronDown, FaCar, FaUser, FaUsers, FaCarAlt } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 import i18next from 'i18next';
 import Cookies from 'js-cookie';
